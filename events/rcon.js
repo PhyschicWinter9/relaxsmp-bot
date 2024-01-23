220818c42bc32ea44e0617cf0d9ed48b5786d06a
PhyschicWinter9 <suratlhaodee@gmail.com>
2024-01-23 10:29:18 +0700

    fix : improve config and module rcon,ready

--- events/rcon.js
+++ events/rcon.js
@@ -6,22 +6,31 @@ module.exports = {
 	name: Events.ClientReady,
 	async execute(interaction){
         const rcon = new Rcon({
-            host: "203.159.92.198",
-            port: 25575,
-            password: "0908672636"
+            host: process.env.RCON_HOST,
+            port: process.env.RCON_PORT,
+            password: process.env.RCON_PASSWORD,
         })
+
+
     
         try {
+            console.log("====================================");
+            console.log("RCON_HOST: " + process.env.RCON_HOST);
+            console.log("RCON connecting...");
+            console.log("");
+
             rcon.on("connect", () => console.log("RCON connected"));
             rcon.on("authenticated", () => console.log("RCON authenticated"));
-            rcon.on("end", () => console.log("RCON connection ended"));
+            // rcon.on("end", () => console.log("RCON connection ended"));
 
             await rcon.connect();
 
             // Log some information about the Minecraft server
-            console.log(await rcon.send("list"));
+            // console.log(await rcon.send("list"));
 
             // You can add more RCON commands or interact with the server here
+            console.log("RCON Connect Success!");
+
 
         } catch (error) {
             console.error("Error interacting with RCON:", error);
--- events/ready.js
+++ events/ready.js
@@ -4,6 +4,9 @@ module.exports = {
 	name: Events.ClientReady,
 	once: true,
 	execute(client) {
+		console.log("====================================");
 		console.log(`Ready! Logged in as ${client.user.tag}`);
+		console.log("====================================");
+		console.log("")
 	},
 };
